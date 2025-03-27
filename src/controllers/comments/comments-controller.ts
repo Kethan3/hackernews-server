63a3583bed0f7720232c7321257f40a739a03433
Kethan3 <kethangowda003@gmail.com>
2025-03-27 16:29:55 +0530

    implement comment functionality: add create, delete, get, and update comments on posts

---
+++ src/controllers/comments/comments-controller.ts
@@ -0,0 +1,55 @@
+import { prismaClient } from "../../extras/prisma";
+import type { CreateCommentResult, GetCommentsResult, UpdateCommentResult } from "./comments-types";
+
+export const getCommentsOnPost = async (parameters: {
+  postId: string;
+  page: number;
+}): Promise<GetCommentsResult> => {
+  const limit = 10;
+  const offset = (parameters.page - 1) * limit;
+
+  const comments = await prismaClient.comment.findMany({
+    where: {
+      postId: parameters.postId,
+    },
+    orderBy: { createdAt: "desc" },
+    skip: offset,
+    take: limit,
+  });
+  return { comments };
+};
+
+export const createComment = async (parameters: {
+  userId: string;
+  postId: string;
+  content: string;
+}): Promise<CreateCommentResult> => {
+  const comment = await prismaClient.comment.create({
+    data: {
+      userId: parameters.userId,
+      postId: parameters.postId,
+      content: parameters.content,
+    },
+  });
+
+  return { comment };
+};
+
+export const deleteComment = async (parameters: {
+  userId: string;
+  commentId: string;
+}): Promise<void> => {
+  const comment = await prismaClient.comment.delete({
+    where: {
+      id: parameters.commentId,
+      userId: parameters.userId,
+    },
+  });
+};
+
+
+export const updateComment = async (parammeters :{
+
+}) : Promise<UpdateCommentResult>{
+    // update comment logic here
+}
