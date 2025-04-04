affc5c05b7b0271ee24d7d65123dd7369c091e92
Kethan3 <kethangowda003@gmail.com>
2025-04-04 11:33:21 +0530

    remove unused authentication, comments, likes, posts, and users controllers and related types

--- dist/src/controllers/posts/posts-types.js
+++
@@ -1,4 +0,0 @@
-export var createPostError;
-(function (createPostError) {
-    createPostError[createPostError["UNKNOWN"] = 0] = "UNKNOWN";
-})(createPostError || (createPostError = {}));
--- dist/src/index.js
+++
@@ -1,5 +0,0 @@
-import "dotenv/config";
-import { serve } from "@hono/node-server";
-import { allRoutes } from "./routes/routes";
-serve(allRoutes);
-console.log("server is running at http://localhost:3000/");
