fd4de69c4404af515d1a36cae41a9565eab3ea33
Kethan3 <kethangowda003@gmail.com>
2025-04-08 09:47:13 +0530

    refactor: improve formatting of environment variable checks for readability

--- src/environment.ts
+++ src/environment.ts
@@ -1,14 +1,20 @@
 // export const jwtsecretKey = process.env.JWT_SECRET_KEY || process.exit(1);
 
-export const jwtSecretKey = process.env.JWT_SECRET_KEY || (() => { 
+export const jwtSecretKey =
+  process.env.JWT_SECRET_KEY ||
+  (() => {
     console.error("JWT_SECRET_KEY is not set! Exiting...");
     process.exit(1);
   })();
-  export const databaseUrl = process.env.DATABASE_URL || (() => { 
+export const databaseUrl =
+  process.env.DATABASE_URL ||
+  (() => {
     console.error("DATABASE_URL is not set! Exiting...");
     process.exit(1);
   })();
-  export const directUrl = process.env.DIRECT_URL || (() => { 
+export const directUrl =
+  process.env.DIRECT_URL ||
+  (() => {
     console.error("DIRECT_URL is not set! Exiting...");
     process.exit(1);
   })();
