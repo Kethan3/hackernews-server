496c0d506f5f72dd73d976f99b2d4df6c293b461
Kethan3 <kethangowda003@gmail.com>
2025-04-08 09:20:23 +0530

    fix: update token authentication method in middleware and OpenAPI documentation

--- src/routes/middlewares/token-middleware.ts
+++ src/routes/middlewares/token-middleware.ts
@@ -1,5 +1,3 @@
-// 
-
 import { createMiddleware } from "hono/factory";
 import jwt from "jsonwebtoken";
 import { jwtSecretKey } from "../../environment.js";
@@ -9,13 +7,16 @@ export const tokenMiddleware = createMiddleware<{
     userId: string;
   };
 }>(async (context, next) => {
-  const authHeader = context.req.header("Authorization");
+  const token = context.req.header("token");
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return context.json({ message: "missing Token" }, 401);
+  if (!token) {
+    return context.json(
+      {
+        message: "missing Token",
+      },
+      401
+    );
   }
-
-  const token = authHeader.split(" ")[1];
 
   try {
     const payload = jwt.verify(token, jwtSecretKey) as jwt.JwtPayload;
