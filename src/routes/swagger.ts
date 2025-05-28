74786e8f02f57cf2889ea30315f046aac369b12b
Kethan3 <kethangowda003@gmail.com>
2025-05-28 14:29:58 +0530

    remove swagger routes for cleanup and consolidation

--- src/routes/swagger.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from "hono";
-import { openapi } from "../docs/openapi.js";
-import { swaggerUI } from "@hono/swagger-ui";
-
-export const swaggerRoutes = new Hono();
-
-swaggerRoutes.get("/doc", (c) => c.json(openapi));
-swaggerRoutes.get("/ui", swaggerUI({ url: "/doc" }));
